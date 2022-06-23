e2b28ebf8ff553db7a3eefc13d361de59c4d9573
Victor Dogo <victor.dogo@predify.me>
2022-06-22 21:24:14 -0300

    adicao do codigo dos itens 2 e 3

--- codigo.R
+++ codigo.R
@@ -26,4 +26,49 @@ for (p in 1:30) {
   }
 }
 
+tbl_result <- tibble(
+  p=1:30,
+  mse=as.double(1:30)
+)
+
+
+
+for (p in 1:length(formulas)) {
+
+  erros <- NULL
+
+  model <- NULL
+
+  for (ii in 1:nrow(df)) {
+
+    model <- lm(formulas[[p]], data=df[-ii,])
+
+    erros <- c(erros,
+               (df[ii,1] - predict(model, df[ii,]))^2)
+  }
+
+  tbl_result[p,2] <- mean(unlist(erros))
+
+}
+
+## ITEM 3
+
+tbl_result |>
+  ggplot()+
+  aes(x=p,y=mse)+
+  geom_line(size=1.25)+
+  theme_minimal()+
+  labs(x = 'P', y = "Mean Squared Error",
+       title='Estimativa da Função de Risco x Valor de P')
+
+tbl_result |>
+  ggplot()+
+  aes(x=p,y=mse)+
+  geom_line(size=1.25)+
+  geom_label(aes(label=paste0('mse = ', round(mse,3))))+
+  coord_cartesian(ylim=c(40,50), xlim=c(1,6))+
+  scale_x_continuous(breaks = 1:6)+
+  theme_minimal()+
+  labs(x = 'P', y = "Mean Squared Error",
+       title='Estimativa da Função de Risco x Valor de P')
 
