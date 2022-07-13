d365eb0be1779988e031f5ec267164c1c2560ab0
Victor Dogo <victor.dogo@predify.me>
2022-07-12 21:54:40 -0300

    codigo teste lista 2

--- lista2/cod2.R
+++ lista2/cod2.R
@@ -5,6 +5,7 @@ set.seed(727838)
 library(tidyverse)
 library(rsample)
 library(glmnet)
+library(ggrepel)
 
 df <- ISLR::Carseats
 df$ShelveLoc <- ifelse(df$ShelveLoc=='Bad', as.factor(0),
@@ -24,12 +25,60 @@ y_tre <- tre[,1]
 x_tes <- tes[,-1] |> as.matrix()
 y_tes <- tes[,1]
 
+# x_tre <- model.matrix(~.-1, tre[,-1])
+# y_tre <- tre[,1] |> pull()
+#
+# x_tes <- model.matrix(~.-1, tes[,-1])
+# y_tes <- tes[,1] |> pull()
+
 # ITEM 2
+
+## Ajuste Minimos Quadrados
 
 ajuste_mq <- glmnet(x_tre, y_tre, alpha=0, lambda=0)
 
+## Ajuste Lasso
+
 cv_lasso <- cv.glmnet(x_tre, y_tre, alpha=1)
-ajuste_lasso <- glmnet(x_tre, y_tre, alpha=1)
+ajuste_lasso <- glmnet(x_tre, y_tre, alpha=1, lambda = cv_lasso$lambda.1se)
 
+## Apresentando melhor lambda (cv_lasso$lambda.1se)
 
+lambdas <- cv_lasso$glmnet.fit$lambda
 
+lam <- lambdas |>
+  as.data.frame() |>
+  mutate(penalty=names(cv_lasso$glmnet.fit$a0)) %>%
+  rename(lambda=1)
+
+results <- cv_lasso$glmnet.fit$beta |>
+  as.matrix() |>
+  as.data.frame() |>
+  rownames_to_column() |>
+  gather(penalty,coefficients,-rowname) |>
+  left_join(lam)
+
+result_labels <- results |>
+  group_by(rowname) |>
+  filter(lambda==ajuste_lasso$lambda) |>
+  ungroup()
+
+ggplot()+
+  geom_line(data=results, aes(lambda, coefficients,
+                              group=rowname, color=rowname), show.legend = FALSE)+
+  scale_x_log10()+
+  geom_text(data=result_labels, aes(0.01, coefficients,
+                                    label=rowname, color=rowname),
+            nudge_x=-.06, show.legend = FALSE)+
+  geom_vline(xintercept = ajuste_lasso$lambda)+
+  annotate(geom = 'text', y=0.15, x=0.3,
+           label=paste0('lambda = ', round(ajuste_lasso$lambda,5)))+
+  theme_minimal()
+
+# ITEM 3
+
+coefficients(ajuste_mq) |>
+  round(3)
+
+coefficients(ajuste_lasso) |>
+  round(3)
